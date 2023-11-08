f6ca835302360fedea09b0794a2d828ec6b80b3d
mohammadhasandabbaghy <m.dabbaghy@gmail.com>
2023-11-08 09:27:01 +0330

    Create LettersFragment.kt

---
+++ app/src/main/java/ir/mhd/words/LettersFragment.kt
@@ -0,0 +1,61 @@
+package ir.mhd.words
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import ir.mhd.words.databinding.FragmentLettersBinding
+
+class LettersFragment: Fragment() {
+
+    private var _binding: FragmentLettersBinding? = null
+    private val binding: FragmentLettersBinding
+        get() = _binding!!
+
+    private var isLinearLayout: Boolean = true
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLettersBinding.inflate(inflater, container, false)
+        setupViews()
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setupViews(){
+        setupRecyclerView()
+        setupLayoutManagerButton()
+    }
+
+    private fun setupRecyclerView() {
+        val letters = ('A'..'Z').map { it.toString() }
+        val adapter = LetterAdapter(letters) {
+
+        }
+        binding.rvLetter.adapter = adapter
+    }
+
+    private fun setupLayoutManagerButton() {
+        binding.ivChangeLayout.setOnClickListener {
+            if (isLinearLayout) {
+                binding.rvLetter.layoutManager = GridLayoutManager(requireContext(), 4)
+                binding.ivChangeLayout.setImageResource(R.drawable.ic_leanear_layout)
+            } else {
+                binding.rvLetter.layoutManager = LinearLayoutManager(requireContext())
+                binding.ivChangeLayout.setImageResource(R.drawable.ic_grid_layout)
+            }
+            isLinearLayout = !isLinearLayout
+        }
+    }
+}
